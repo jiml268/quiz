87ba9155be2eddd8e91f24d5e18bcf6dd346c5a1
jiml268 <114753310+jiml268@users.noreply.github.com>
2024-05-05 13:49:51 -0400

    change backend url from local host

--- src/redux/quiz/quizOperators.js
+++ src/redux/quiz/quizOperators.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
-axios.defaults.baseURL = "http://localhost:3030/api/quiz";
-// axios.defaults.baseURL = "https://quiz-backend-zi2r.onrender.com/api/quiz"
+//axios.defaults.baseURL = "http://localhost:3030/api/quiz";
+ axios.defaults.baseURL = "https://quiz-backend-zi2r.onrender.com/api/quiz"
 
 export const getCategories = createAsyncThunk(
   "/getCategories",
