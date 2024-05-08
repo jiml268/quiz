616b4bf16c96af59f3055288f1c54b2f752cf1fe
jiml268 <114753310+jiml268@users.noreply.github.com>
2024-05-08 08:46:25 -0400

    work on readme.md file

--- src/components/Home/Description/Description.jsx
+++ src/components/Home/Description/Description.jsx
@@ -4,7 +4,7 @@ function Description() {
  return (
      <>
          <h1 className={styles.title}> Welcome to <br /> Mind Bending Quizzes</h1>
-         <p className={styles.description}>Mind Bending Quizzes is a place to put your mind to the test.  With over 20 Categories and over 19,000 question we will test your brain to the fullest.</p>
+         <p className={styles.description}>Mind Bending Quizzes is a place to put your mind to the test.  With over 20 Categories and over 19,000 questions we will test your brain to the fullest.</p>
      </>
  )
 }
